49a770461db6806fc88f112b96b360e0546bf390
Kevin Johnson <kiseleon@users.noreply.github.com>
2015-04-30 21:20:57 -0600

    Create clue.php

---
+++ clue/clue.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <link rel="icon" href="favicon.ico" type="image/x-icon" />
+
+    <title>Kevin Johnson - Assignments</title>
+
+    <!-- Bootstrap -->
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="/css/cover.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <body>
+
+    <div class="site-wrapper">
+
+      <div class="site-wrapper-inner">
+
+        <div class="cover-container">
+
+          <div class="masthead clearfix">
+            <div class="inner">
+              <h3 class="masthead-brand">Kevin Johnson</h3>
+              <nav>
+                <ul class="nav masthead-nav">
+                  <li><a href="/index.html">Home</a></li>
+                  <li><a href="/assignments.html">Assignments</a></li>
+                  <li><a href="/aboutme.html">About Me</a></li>
+                  <li class="active"><a href="#">Clue</a></li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+
+          <div class="inner cover">
+            <h1 class="cover-heading">Clue.</h1>
+            <p class="lead">The classic board game, digital.</p>
+            <p class="lead">
+              <a href="#" class="btn btn-lg btn-default">Manage Players</a>
+              <a href="#" class="btn btn-lg btn-default">Manage Games</a>
+              <a href="#" class="btn btn-lg btn-default">Play!</a>
+            </p>
+          </div>
+
+          <div class="mastfoot">
+            <div class="inner">
+              <p>Powered by <a href="http://getbootstrap.com">Bootstrap</a>.</p>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
