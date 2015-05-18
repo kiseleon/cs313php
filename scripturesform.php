085f0a346780867ce4f1d706daa741058fb29e73
Kevin Johnson <kiseleon@users.noreply.github.com>
2015-05-18 16:24:53 -0600

    Create scripturesform.php

---
+++ scripturesform.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>Scripture Search</title>
+</head>
+
+<body>
+<form action="scriptures.php" method="POST">
+<p>Enter the book to search for: <input type="input" name="book" /></p>
+<input type="submit" />
+</form>
+</body>
+
+</html>
